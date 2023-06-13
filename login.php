1ac62eab50a9442f256c01ddc437152cf3cd9ec2
Salaheddine Chouikh <76973236+Galuyoo@users.noreply.github.com>
2023-06-13 16:37:00 +0100

    Update login.php
    few changes to the login.php file to match what i did to index.php

--- login.php
+++ login.php
@@ -1,39 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE-edge">
-        <meta name="vewport" content="width=device-width, initial-scale=1.0">   
-        <title>
-            login
-        </title>
-        <link rel="stylesheet" href="./static/login.css">
-    </head>
-    <body>
-        <div class="banner">
-            <div class="logo_buttons">
-                <img src="./static/agamine_logo.png" class="logo">
-                <ul>
-                    <li><a href="index.php">home</a></li>
-                </ul>
-            </div>
-            <div class="form-box">
-                <form action="test_.php" class="input-group" method="post">
-                    <?php if(isset($_GET['error'])) { ?>
-                        <p class="error"> <?php echo $_GET['error']; ?></p>
-                    <?php } ?>
-                    <input  type="text" class="input-field" placeholder="user name" name="username" style="color:#fff;"
-                    required>
-                    <input type="password" class="input-field" placeholder="pass word" name="password" required>
-                    <ul>
-                        <li><a href="signup.php">did you forget your password ?</a></li>
-                    </ul>
-                    <!--<input type="checkbox" class="check-box"><snap>did you forget your pass word ?</snap>-->
-                    <!--<button type="submit" class="submit-btn">log in</button>-->
-                    <input type="submit" class="submit-btn" value="login">
-                </form>  
-            </div>
-        </div>
-        
-    </body>
-</html>
+<?php      
+    include('conn.php');  
+    $username = $_POST['user'];  
+    $password = $_POST['pass'];  
+      
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select *from login where username = '$username' and password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+            // Redirect to the home page or desired page
+            header("Location: home.php");
+            exit(); 
+            // Make sure to exit after redirection
+
+        }  
+        else{  
+            echo "<h1> Login failed. Invalid username or password.</h1>";  
+            header("Location: index.php");
+            exit(); 
+
+        }     
+?>  
